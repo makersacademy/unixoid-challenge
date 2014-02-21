1727d519f1a6765eebdfd822ea30d81f48cc4e58
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2014-02-21 11:04:30 +0000

    relaxed one regex to be case-insensitive

--- unixoid_spec.rb
+++ unixoid_spec.rb
@@ -27,7 +27,7 @@ describe "Unixoid test" do
 
   it "should have the list of env vars that begin with T in t-vars.env" do
     lines = File.readlines('my/private/files/t-vars.env')
-    expect(lines.shift).to match(/^List of env vars that begin with T$/)    
+    expect(lines.shift).to match(/^List of env vars that begin with T$/i)    
     expect(lines.map{|l| l[0]}.uniq).to eq(['T'])
   end
 
