40bde184dd48da3509f39f1bd78fc6b885312aba
Sam Joseph <tansaku@gmail.com>
2014-10-13 17:49:42 +0100

    adds gem and support for automated submission of results

---
+++ lib/unixoid_challenge.rb
@@ -0,0 +1,20 @@
+require 'byebug'
+
+class UnixoidChallenge
+  def self.run
+    puts "Running the Unixoid Challenge Assessor ..."
+    File.write 'unixoid_spec.rb', File.readlines(File.join(File.dirname(__FILE__), '..','spec','unixoid_spec.rb')).join
+    `rspec unixoid_spec.rb > unixoid_results.txt`
+    puts 'Please enter your github user name'
+    username = gets.chomp
+    curl = %Q{curl -u #{username} https://api.github.com/user/repos -d '{"name":"unixoid_submission"}'}
+    %x[#{curl}]
+    `git init`
+    `git add unixoid_results.txt`
+    `git commit -am 'unixoid submission'`
+    add_remote = "git remote add origin git@github.com:#{username}/unixoid_submission.git"
+    %x[#{add_remote}]
+    `git push origin master`
+    puts "Completed Submission!"
+  end
+end
---
+++ unixoid-challenge.gemspec
@@ -0,0 +1,22 @@
+Gem::Specification.new do |s|
+  s.name        = 'unixoid-challenge'
+  s.version     = '0.0.2'
+  s.date        = '2014-10-13'
+  s.summary     = "Test your command line skills!"
+  s.description = "A gem to assess your performance on the unixoid challenge"
+  s.authors     = ["Sam Joseph", "Dan Le Dosquet-Bergquist"]
+  s.email       = 'sam@makersacademy.com'
+  s.files       = %w[
+    bin/unixoid-challenge
+    lib/unixoid_challenge.rb
+    spec/unixoid_spec.rb
+  ]
+  s.homepage    =
+    'http://makersacademy.com'
+
+  ## This gets added to the $LOAD_PATH so that 'lib/NAME.rb' can be required as
+  ## require 'NAME.rb' or'/lib/NAME/file.rb' can be as require 'NAME/file.rb'
+  s.require_paths = %w[lib]
+
+  s.executables = ["unixoid-challenge"]
+end
