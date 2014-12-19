a07317ccc20ebda737e84a271f608e0adcd04c4c
Spike Nox <spike01@gmail.com>
2014-12-19 10:36:17 +0000

    Exits on auth fail. Closes #15

--- lib/unixoid.rb
+++ lib/unixoid.rb
@@ -8,39 +8,54 @@ require 'unixoid/challenge_result'
 module Unixoid
 
   def self.run
-    github = Github.create_repo
-    return auth_fail unless github.authenticated?
-
-    challenge = Challenge.run_specs
-    results = ChallengeResult.new(challenge)
-
-    puts 'Checking and submitting...'
-    Git.submit(github)
-
-    puts render_results(results)
-    puts 'Challenge Submitted!'
+    create_github_repos
+    final_result(run_challenge)
   end
 
   private
 
-  def self.auth_fail
-    puts 'Incorrect login details. Please run unixoid-challenge again'.red 
-  end
+  class << self
 
-  def self.render_results(results)
-    case results.status
-    when :complete
-      'Congratulations!'.green
-    when :attempted
-      %{
+    def create_github_repos
+      github = Github.create_repo
+      auth_fail! unless github.authenticated?
+    end
+
+    def run_challenge
+      challenge = Challenge.run_specs
+      ChallengeResult.new(challenge)
+    end
+
+    def check_and_submit
+      puts 'Checking and submitting...'
+      Git.submit(github)
+    end
+
+    def final_result(results)
+      puts render_results(results)
+      puts 'Challenge Submitted!'
+    end
+
+    def auth_fail!
+      puts 'Incorrect login details. Please run unixoid-challenge again'.red 
+      exit 1
+    end
+
+    def render_results(results)
+      case results.status
+      when :complete
+        'Congratulations!'.green
+      when :attempted
+        %{
 Almost there! Feel free to try again and re-submit
 
 The following answers need to be corrected:
 
-#{results.failures.join(", ")}
-      }.yellow
-    when :unattempted
-      'Looks like you have not tried the challenge. Give it a go and resubmit'.red
+          #{results.failures.join(", ")}
+        }.yellow
+      when :unattempted
+        'Looks like you have not tried the challenge. Give it a go and resubmit'.red
+      end
     end
   end
 end
