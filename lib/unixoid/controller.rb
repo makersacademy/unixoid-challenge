ab6aff6542163d67d5b078e0c973e6a600c7637d
Spike Nox <spike01@gmail.com>
2015-01-06 13:53:07 +0000

    Typo

--- lib/unixoid/controller.rb
+++ lib/unixoid/controller.rb
@@ -43,7 +43,7 @@ module Unixoid
         puts render_results(results)
       end
 
-      def get_config
+      def get_config(git)
         puts 'Please enter your full name:'
         name = gets.chomp        
         puts 'Please enter your e-mail address:'
